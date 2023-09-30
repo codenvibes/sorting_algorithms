81939823310be6844d6c466d9504de290dd35253
codenvibes <terrymk1999@gmail.com>
2023-09-30 23:24:22 +0300

    Create a function that sorts an array of integers in ascending order using Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,95 @@
+#include "sort.h"
+
+/*
+ * Auth: codenvibes
+ */
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * lomuto_partition - Partitions an array using the Lomuto partition scheme.
+ * @array: Pointer to the array to be partitioned.
+ * @size: Number of elements in the array.
+ * @left: Index of the left boundary of the partition.
+ * @right: Index of the right boundary of the partition.
+ *
+ * Return: The final pivot position after partitioning.
+ */
+
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Recursively sorts an array using the Lomuto partition scheme.
+ * @array: Pointer to the array to be sorted.
+ * @size: Number of elements in the array.
+ * @left: Index of the left boundary of the subarray to be sorted.
+ * @right: Index of the right boundary of the subarray to be sorted.
+ */
+
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using quicksort.
+ * @array: Pointer to the array to be sorted.
+ * @size: Number of elements in the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
